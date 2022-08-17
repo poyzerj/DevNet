aecf1fee4c17f7e41cfdb13c767f3696832596ad
Joe Poyzer <poyzerj@gmail.com>
2022-08-16 23:21:12 -0500

    fixed nxapirealworld.py script

--- Nexus API/nxapirealworld.py
+++ Nexus API/nxapirealworld.py
@@ -1,7 +1,6 @@
-from urllib import response
 import requests
 import json
-from pprint import pprint
+import re
 
 switchuser = 'admin'
 switchpassword = "K1pe3kjt!"
@@ -23,7 +22,7 @@ payload = {
   }
 }
 
-response = requests.post(url, data=json.dumps(payload), headers=myheaders, auth=(switchuser, switchpassword), verify=False).json
+response = requests.post(url, data=json.dumps(payload), headers=myheaders, auth=(switchuser, switchpassword), verify=False).json()
 #print(response)
 
 ############################### LOGIN WITH NX-API REST
@@ -49,18 +48,26 @@ cookies['APIC-cookie'] = token
 
 counter = 0
 nei_count = response['ins_api']['outputs']['output']['body']['neigh_count']
+
 print(nei_count)
 
 while counter < nei_count:
-    hostname = ['ins_api']['outputs']['output']['body']['TABLE_cdp_neighbor_brief_info']['ROW_cdp_neighbor_brief_info'][counter]['device_id']
-    local_int = ['ins_api']['outputs']['output']['body']['TABLE_cdp_neighbor_brief_info']['ROW_cdp_neighbor_brief_info'][counter]['intf_id']
-    remote_int = ['ins_api']['outputs']['output']['body']['TABLE_cdp_neighbor_brief_info']['ROW_cdp_neighbor_brief_info'][counter]['port_id']
+    hostname = response['ins_api']['outputs']['output']['body']['TABLE_cdp_neighbor_brief_info']['ROW_cdp_neighbor_brief_info']['device_id']
+    local_int = response['ins_api']['outputs']['output']['body']['TABLE_cdp_neighbor_brief_info']['ROW_cdp_neighbor_brief_info']['intf_id']
+    remote_int = response['ins_api']['outputs']['output']['body']['TABLE_cdp_neighbor_brief_info']['ROW_cdp_neighbor_brief_info']['port_id']
 
     body = {
         "l1PhysIf": {
             "attributes": {
-                "descr": 'Connected to " '+hostname+' Remote interface is '+remote_int+'
+                "descr": 'Connected to '+hostname+' Remote interface is '+remote_int+''
             }
         }
     }
     counter += 1
+
+    if local_int != 'mgmt0':
+        int_name = str.lower(str(local_int[:3]))
+        int_num = re.search(r'[1-9]/[1-9]*', local_int)
+        int_url = 'https://192.168.1.251/api/mo/sys/intf/phys-['+int_name+str(int_num.group(0))+'].json'
+        post_response = requests.post(int_url, data=json.dumps(body), headers=myheaders, cookies=cookies, verify=False).json()
+        print(post_response)
